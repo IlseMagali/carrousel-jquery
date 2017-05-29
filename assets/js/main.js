3ba9d63558855222898bf86d71ddeb8e80f88c8c
IlseMagali <ilsemke@gmail.com>
2017-05-29 16:37:52 +0000

    probando con jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
-jQuery(document).ready(function(){
-});
+// $(document).ready(function(){
+//   $("#siguiente").click(function(){
+//     $("#imagenUno").css("visibility", "none");
+//     $("#imagenDos").css("visibility", "visible")
+//   });
+// });
 
 $(document).ready(function(){
-  $("#siguiente").click(function(event){
-    $("#imagenUno").fadeIn();
-  });
-});
-
-$(document).ready(function(){
-  $("#anterior").click(function(event){
-    $("#imagenDos").fadeIn(4000);
-  });
+	// Set the interval to be 5 seconds
+	var t = setInterval(function(){
+		$("#carrousel ul").animate({marginLeft:0},1000)
+      $("#carrousel ul").animate({marginLeft:0},1000,function(){
+  			$(this).find("li:last").after($(this).find("li:first"));
+  			$(this).css({marginLeft:0});
+		  })
+	},5000);
 });
