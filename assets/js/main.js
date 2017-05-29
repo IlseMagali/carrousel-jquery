684dd67ed71830a66e95dd297a9dbd937cd80cbe
IlseMagali <ilsemke@gmail.com>
2017-05-29 15:24:54 +0000

    carrousel en construcción

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function(){
+});
+
+$(document).ready(function(){
+  $("#siguiente").click(function(event){
+    $("#imagenUno").fadeIn();
+  });
+});
+
+$(document).ready(function(){
+  $("#anterior").click(function(event){
+    $("#imagenDos").fadeIn(4000);
+  });
+});
